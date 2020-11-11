156b43349997b0c538d6b456589d502c0de4e9ad
Nikita Rysiev <nickpolska21@gmail.com>
2020-11-11 13:45:44 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let changeBackground = document.querySelector(".js-changeBackground");
+let container = document.querySelector(".container")
+let lightBlueBackground = document.querySelector(".lightBlueBackground");
+let nextColorName = document.querySelector(".js-nextColorName");
+
+changeBackground.addEventListener("click", () => {
+  container.classList.toggle("lightBlueBackground");
+  nextColorName.innerText = container.classList.contains("lightBlueBackground") ? "lightgray" : "lightblue";
+});
