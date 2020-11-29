e23a6a9cd8c06fa75030df5222e8045cc86119d2
Nikita Rysiev <nickpolska21@gmail.com>
2020-11-29 19:03:04 +0100

    added function which change underline color

--- js/script.js
+++ js/script.js
@@ -3,18 +3,20 @@
     console.log("Hello to all developers, who visit here 🔒")
   };
   
+  
   const toggleBackground = () => {
     const container = document.querySelector(".container")
     const nextColorName = document.querySelector(".js-nextColorName");
-
+    const underline = document.querySelector(".underline")
+    
     container.classList.toggle("whiteBackground");
     nextColorName.innerText = container.classList.contains("whiteBackground") ? "lightgray" : "white";
+    underline.classList.toggle("grayUnderline"); 
   };
   
   
   const init = () => {
     const changeBackground = document.querySelector(".js-changeBackground");
-    
     changeBackground.addEventListener("click", toggleBackground);
     
     welcome();
