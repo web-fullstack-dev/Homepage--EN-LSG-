43ef7e4aa12d66089d302da6727a3301f3334911
Nikita Rysiev <nickpolska21@gmail.com>
2020-11-29 20:10:16 +0100

    added function for underline, name and image color

--- js/script.js
+++ js/script.js
@@ -7,11 +7,28 @@
   const toggleBackground = () => {
     const container = document.querySelector(".container")
     const nextColorName = document.querySelector(".js-nextColorName");
-    const underline = document.querySelector(".underline")
+    // _________________
+    const navColor = document.querySelector(".navColor");
+    // _________________
+    const imageColor = document.querySelector(".imageColor");
+    const nameColor = document.querySelector(".nameColor");
+    const underlineColor = document.querySelector(".underlineColor");
+    // _________________
+    const footerColor = document.querySelector(".footerColor");
+    // _________________
     
     container.classList.toggle("whiteBackground");
-    nextColorName.innerText = container.classList.contains("whiteBackground") ? "lightgray" : "white";
-    underline.classList.toggle("grayUnderline"); 
+    nextColorName.innerText = container.classList.contains("whiteBackground") ? "lightblue" : "white";
+    
+    // __________________
+    navColor.classList.toggle("lightBlueNavColor");
+    // __________________
+    imageColor.classList.toggle("grayColorImage");
+    nameColor.classList.toggle("grayColorName");
+    underlineColor.classList.toggle("grayColorUnderline"); 
+    // __________________
+    footerColor.classList.toggle("lightBlueFooterColor");
+    // __________________
   };
   
   
