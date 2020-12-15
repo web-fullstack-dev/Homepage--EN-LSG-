9883f5a20cccc191776d4ce4ec3cc47a960fe33d
Nikita Rysiev <nickpolska21@gmail.com>
2020-12-15 19:16:30 +0100

    deleted tables about my courses

--- js/script.js
+++ js/script.js
@@ -8,16 +8,12 @@
     const container = document.querySelector(".container")
     const nextColorName = document.querySelector(".js-nextColorName");
 
-    const navColor = document.querySelector(".navColor");
+    // const navColor = document.querySelector(".navColor");
     const imageColor = document.querySelector(".imageColor");
     const nameColor = document.querySelector(".nameColor");
     const underlineColor = document.querySelector(".underlineColor");
-
-    const underlineColor__color__lightblue = document.querySelector(".underlineColor__color__lightblue");
-    const underlineColor__colorLightBlue = document.querySelector(".underlineColor__colorLightBlue");
-    const underlineColor__colorLight = document.querySelector(".underlineColor__colorLight");
-    // _________________
-    const footerColor = document.querySelector(".footerColor");
+    // _______________
+    // const footerColor = document.querySelector(".footerColor");
     // _________________
     
     container.classList.toggle("whiteBackground");
@@ -29,11 +25,8 @@
     nameColor.classList.toggle("lightblueColorName");
     underlineColor.classList.toggle("lightblueColorUnderline"); 
 
-    underlineColor__color__lightblue.classList.toggle("lightblueColorUnderline"); 
-    underlineColor__colorLightBlue.classList.toggle("lightblueColorUnderline"); 
-    underlineColor__colorLight.classList.toggle("lightblueColorUnderline"); 
     // __________________
-    footerColor.classList.toggle("lightblueFooterColor");
+    // footerColor.classList.toggle("lightblueFooterColor");
     // __________________
   };
   
